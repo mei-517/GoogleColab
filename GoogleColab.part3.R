e37d5ddb08076aed7e2e946c170195689c3267fe
mei-517 <meisuma@icloud.com>
2026-09-02 17:24:04 +0900

    add 1-3 20260902

--- GoogleColab.part3.R
+++ GoogleColab.part3.R
@@ -4,12 +4,41 @@
 ## Date:2026/08/31
 ## 第１部３章
 
-install.packeges("e1071")
+utils::install.packages("e1071")
 library(e1071)
 
 ## dataを取得
-d <- read.table("elephant_data.txt",header = FALSE)
+setwd("E:/home/nagai/workspace/GoogleColab")
+df <- read.table("elephant_data.txt",header=F)
 
 ## Xd:体長体重 yd:ラベル
-Xd <- d[,1:2]
-yd <- d[,3]
+Xd <- df[,1:2]
+yd <- df[,3]
+
+head(Xd)
+head(yd)
+
+## def min_max
+min_max <- function(x1,x2){
+  
+  min_val <- apply(x2,2,min)
+  max_val <- apply(x2,2,max)
+  
+  result <- (x1 - min_val)/(max_val - min_val)
+  return(result)
+  
+}
+
+min_max(Xd,Xd)
+
+## def min_max python 通りver
+## 後で確認################################################################
+
+# x1 <- t(x1)
+# x2 <- t(x2)
+# min_max <- function(x1,x2){
+# min_val <- apply(x2,1,min)
+# max_val <- apply(x2,1,max)
+# result <- (x1 - min_val)/(max_val - min_val)
+# return(t(result))
+# }
