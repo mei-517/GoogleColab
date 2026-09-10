ec27ae4064484c9a9493c9caa5d75362c95da93f
mei-517 <meisuma@icloud.com>
2026-09-10 18:48:05 +0900

    add 1-3 20260910

--- GoogleColab.part3.R
+++ GoogleColab.part3.R
@@ -29,7 +29,7 @@ setwd("E:/home/nagai/workspace/GoogleColab")
 d <- read.table("elephant_data.txt",header=F)
 
 ## Xd:体長体重 yd:ラベル
-Xd <- d[,1:(ncol(d)-1)]
+xd <- d[,1:(ncol(d)-1)]
 yd <- d[,ncol(d)]
 
 # head(Xd)
@@ -39,7 +39,7 @@ yd <- d[,ncol(d)]
 ## 図をかく
 draw_graph <- function(){
   ## マーカー
-  markers <- c(1, 4, 2, 6, 5, 8, 3, 18)
+  markers <- c(0, 1, 2, 3, 4, 5, 6 ,7)
   colors <- c("red", "blue", "green", "black","cyan", "magenta", "yellow", "grey")
   m <- 1
   ## 空の図
@@ -52,6 +52,7 @@ draw_graph <- function(){
   }
 }
 
+draw_graph
 
 ## 図を保存
 png("img1.png")
@@ -87,4 +88,43 @@ y_pred <- predict(model, x)
 ## 正解率
 mean(y_pred == y)
 
-## 
+## 境界線をかく########################################
+
+draw_graph_withline(){
+  
+  ## マーカー
+  markers <- c(0, 1, 2, 3, 4, 5, 6 ,7)
+  colors <- c("red", "blue", "green", "black","cyan", "magenta", "yellow", "grey")
+  m <- 1
+  ## 空の図
+  plot(Xd[, 1], Xd[, 2], type = "n")
+  ## 図示
+  for (i in unique(yd)) {
+    n <- which(yd == i)
+    points(Xd[n, 1], Xd[n, 2], pch = markers[m], col = colors[m])
+    m <- m + 1
+  }
+  
+  ## グラフ範囲
+  x_min <- min(X[, 1])
+  x_max <- max(X[, 1])
+  y_min <- min(X[, 2])
+  y_max <- max(X[, 2])
+  
+  ## 格子状
+  x_seq <- seq(x_min, x_max, length.out = 200)
+  y_seq <- seq(y_min, y_max, length.out = 200)
+  grid <- expand.grid(x = x_seq, y = y_seq)
+  
+  ## SVM
+  z <- predict(model,grid)
+  
+  
+  z_num <- as.numetric(z) -1
+  z_matrix <- matrix(z_num, nrow = length(x_seq), ncol = length(y_seq))
+  
+  ## 背景
+  image(x_seq, y_seq, z_matrix, col = c)
+  
+}
+
